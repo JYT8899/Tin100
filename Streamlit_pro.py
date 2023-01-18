203561e0326ff8fc491e0f9d96edd9e9f24646c6
joel <joel.yacob.teklemariam@nmbu.no>
2023-01-18 12:30:57 +0100

    5 x totalt inntekt regel

--- Streamlit_pro.py
+++ Streamlit_pro.py
@@ -142,6 +142,12 @@ def knapp():
         For å få det ønsket lånet må du ha en egenekapital større eller lik {2} Du kan ikke låne: \
         {0} USD i {1} måneder'.format(Laan * 1000, mnd, (onsk_laan * 1000) * 0.15))
 
+        elif (Inntekt + Medsokerinntekt) >= onsk_laan / 5:
+            st.write('Takk for søknaden, din søknad er dessverre ikke akseptert.'
+                     ' \
+For å få det ønsket lånet må du ha en total inntekt (medsøkerinntekt + inntekt) større eller lik {2} Du kan ikke låne: \
+{0} USD i {1} måneder'.format(Laan * 1000, mnd, (onsk_laan * 1000)/5))
+
         prediction = tin.predic(data, tin.RanForClf())
 
         if prediction == 0:
